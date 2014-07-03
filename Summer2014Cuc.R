8dbb595923ee609b12c6216534850fd00fac555e
Cuc Tran <Cucedough@gmail.com>
2014-07-03 12:54:31 -0400

    Parameters
    Problem with BetaT

---
+++ Summer2014Cuc.R
@@ -0,0 +1,14 @@
+
+#Dushoff et al PNAS Dynamical Influenza Seasonality Excercise 
+
+#Parameteres 
+N <- 500000
+L <- 4 
+D <- 0.02
+Betanot <- 500
+Beta1 <- 0.02
+t <- 4 #?this is a dummy number
+BetaT <- Betanot((1+Beta1)*cos(2*pi*t)) #Betanot((1+Beta1)cos(2*pi*t))
+S <- 4#? this is a dummy number
+I <- 7 #? this is a dummy number
+  
