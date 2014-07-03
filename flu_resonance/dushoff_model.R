0f0adcdd01962f157544494f67ecf301df3ab421
Tom Hladish <tjhladish@ufl.edu>
2014-07-03 17:56:54 -0400

    Tweaks, fiddling with driver period

--- flu_resonance/dushoff_model.R
+++ flu_resonance/dushoff_model.R
@@ -1,21 +1,49 @@
 library(deSolve)
 
-beta = function(b0, b1, t) {
-    b0 * (1 + b1 * cos(2*pi*t) );
+beta = function(t, b0, b1, b2) {
+    b0 * (1 + b1 * cos(b2*2*pi*t) );
 }
 
 model = function(t, y, p) {
-    beta_t = beta(p['b0'], p['b1'], t)
-    ds = (p['n']-y[1]-y[2])/p['l'] - beta_t*y[2]*y[1]/p['n']
-    di = beta_t*y[2]*y[1]/p['n'] - y[2]/p['d']
-    list(c(ds, di))
+    with(as.list(c(y, p)), {
+        beta_t = beta(t, b0, b1, b2)
+        ds = (n-s-i)/l - beta_t*i*s/n
+        di = beta_t*i*s/n - i/d
+        list(c(ds, di))
+    })
 }
 
-
-pars = c(b0 = 500, b1 = 0.02, n = 500000, l = 4, d = 0.02)
-yini = c(s = pars['n'] - 1, i = 1)
+pars = c(b1 = 0.04, b2 = 1, n = 500000)
+yini = c(s = 500000 - 1, i = 1)
 times = seq(from = 0, to = 20, by = 0.01)
+
+pars['b0'] = 500
+pars['l']  = 4
+pars['d']  = 0.02
 out1 = ode (times = times, y = yini, func = model, parms = pars)
 
-pars = c(b0 = 400, b1 = 0.02, n = 500000, l = 8, d = 0.025)
+pars['b0'] = 400
+pars['l']  = 8
+pars['d']  = 0.025
 out2 = ode (times = times, y = yini, func = model, parms = pars)
+
+plot_fig = function(x,y) {
+    plot(x, y, ylim=c(0,4000), type='l', col='red', bty='n', xlab="Time", ylab="I", las=2)
+}
+
+row_subset = 1001:2001
+t = times[row_subset]
+Ia = out1[row_subset,3]
+Ib = out2[row_subset,3]
+
+par(mfrow=c(2,1))
+plot_fig(t,Ia)
+plot_fig(t,Ib)
+
+pars['b2'] = 1.05
+out3 = ode (times = times, y = yini, func = model, parms = pars)
+points(t, out3[row_subset, 3], ylim=c(0,4000), type='l', col='blue', bty='n', xlab="Time", ylab="I", las=2)
+
+pars['b2'] = 0.95 
+out4 = ode (times = times, y = yini, func = model, parms = pars)
+points(t, out4[row_subset, 3], ylim=c(0,4000), type='l', col='green', bty='n', xlab="Time", ylab="I", las=2)
