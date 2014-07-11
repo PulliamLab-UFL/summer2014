9d583cdb9497d923fe03ae3cf89fa45b3ef4ea95
Tom Hladish <tjhladish@ufl.edu>
2014-07-11 04:57:32 -0400

    Finding, changing periods

--- flu_resonance/dushoff_model.R
+++ flu_resonance/dushoff_model.R
@@ -13,9 +13,12 @@ model = function(t, y, p) {
     })
 }
 
+find_peaks = function(x) {which(diff(sign(diff(x)))==-2)+1}
+
 pars = c(b1 = 0.04, b2 = 1, n = 500000)
 yini = c(s = 500000 - 1, i = 1)
-times = seq(from = 0, to = 20, by = 0.01)
+delta = 1/365
+times = seq(from = 0, to = 20, by = delta)
 
 pars['b0'] = 500
 pars['l']  = 4
@@ -29,9 +32,10 @@ out2 = ode (times = times, y = yini, func = model, parms = pars)
 
 plot_fig = function(x,y) {
     plot(x, y, ylim=c(0,4000), type='l', col='red', bty='n', xlab="Time", ylab="I", las=2)
+    abline(v=min(x)+find_peaks(y)*delta, col='#00555555')
 }
 
-row_subset = 1001:2001
+row_subset = which(times>=10)[[1]]:length(times)
 t = times[row_subset]
 Ia = out1[row_subset,3]
 Ib = out2[row_subset,3]
@@ -40,10 +44,13 @@ par(mfrow=c(2,1))
 plot_fig(t,Ia)
 plot_fig(t,Ib)
 
-pars['b2'] = 1.05
-out3 = ode (times = times, y = yini, func = model, parms = pars)
-points(t, out3[row_subset, 3], ylim=c(0,4000), type='l', col='blue', bty='n', xlab="Time", ylab="I", las=2)
+print(table(diff(find_peaks(Ia))))
+print(table(diff(find_peaks(Ib))))
 
-pars['b2'] = 0.95 
-out4 = ode (times = times, y = yini, func = model, parms = pars)
-points(t, out4[row_subset, 3], ylim=c(0,4000), type='l', col='green', bty='n', xlab="Time", ylab="I", las=2)
+#pars['b2'] = 1.05
+#out3 = ode (times = times, y = yini, func = model, parms = pars)
+#points(t, out3[row_subset, 3], ylim=c(0,4000), type='l', col='blue', bty='n', xlab="Time", ylab="I", las=2)
+
+#pars['b2'] = 0.95 
+#out4 = ode (times = times, y = yini, func = model, parms = pars)
+#points(t, out4[row_subset, 3], ylim=c(0,4000), type='l', col='green', bty='n', xlab="Time", ylab="I", las=2)
