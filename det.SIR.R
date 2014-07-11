d5c6b1526cc36c2fe61e6a9dd0988f08b472926a
Carl Pearson <cap10@ufl.edu>
2014-07-11 11:36:46 -0400

    changes to match benchmarks

--- det.SIR.R
+++ det.SIR.R
@@ -23,5 +23,41 @@ plotter<-function(parms) {
   plot(out[,'time'], out[,'I'], type = "l", xlim=c(10,20), ylim=c(0,ymax), ylab="I", xlab="year")
 }
 
-plotter(parmsA)
-plotter(parmsB)
+compare.plots<-function(params, ts, y0, tkeep) {
+  res <- lapply(params, function(p) { ode(y0, ts, sir, p)[,"I"] })
+  res <- Reduce(cbind, res)
+  matplot(ts, res, type="l", xlim=c(tkeep,max(ts)), ylim=c(0, 1.1*max(res[ts > tkeep,])), xlab="years", ylab="I")
+}
+
+compare.plots.y0<-function(params, ts, y0, tmax) {
+  res <- lapply(y0, function(y) { ode(y, ts, sir, params)[,"I"] })
+  res <- Reduce(cbind, res)
+  matplot(ts, res, type="l", xlim=c(0, tmax), ylim=c(0, 1.1*max(res)), xlab="years", ylab="I")
+}
+
+compare.plots(list(parmsA,parmsB), seq(0,20,0.1), y0, 10)
+
+vs.non.seasonal <- function(params, ...) {
+  non.seasonal <- params
+  non.seasonal["b1"] <- 0
+  compare.plots(list(params, non.seasonal), ...)
+}
+
+vs.non.seasonal(parmsA, seq(0,20,0.1), y0, 0)
+
+star <- function(params) with(as.list(params),{
+  S <- N/(b0*D)
+  return(c(S=S,I=(N-S)/(1+L/D)))
+})
+
+period <- function(params) with(as.list(params), 2*pi*sqrt(D*L/(D*b0-1)))
+
+vs.eq.df <- function(params, ts, y0, ...) {
+  y0.df <- c(params["N"]-0.1, 0.1)
+  names(y0.df) <- c("S","I")
+  y0.eq <- star(params)
+  compare.plots.y0(params, ts, list(y0, y0.df, y0.eq), ...)
+}
+
+vs.eq.df(parmsA, seq(0,20,0.1), y0, 5)
+
