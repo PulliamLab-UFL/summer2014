bdba204e6ddc617bda6970ce1dd85a19eed432a8
jenniesuz <jenniesuz@gmail.com>
2014-08-01 13:50:28 -0400

    changed to start at endemic equilibrium

--- flu_stoc_mod2_jl.R
+++ flu_stoc_mod2_jl.R
@@ -5,14 +5,16 @@
 library("GillespieSSA")
 
 # params
-parms<-c(beta=400,inf=0.02,imm=4,N=50000) # parameters
-x0<-c(S=50000-1,I=1,R=0) # initial state
+ptm<-proc.time()
+parms<-c(beta=400,inf=0.025,imm=4,N=50000) # parameters
+x0<-c(S=5000,I=140,R=0) # initial state
 a<-c("beta*I*S/N","I/inf","(N-S-I)/imm") # character vector of propensity functions
 nu<-matrix(c(-1,0,1,1,-1,0,0,1,-1),nrow=3,byrow=TRUE)
 out<-ssa(x0,a,nu,parms,tf=20,method="BTL")
 
 dat<-out$dat
-plot(dat[,1],dat[,3],type="l",xlim=c(0,1))
+plot(dat[,1],dat[,3],type="l",xlim=c(0,20))
+proc.time() -ptm
 
 
 
